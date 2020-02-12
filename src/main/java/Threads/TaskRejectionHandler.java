8c347aac4e0aa0ccac1725e3eb2bb9ecf195884b
AyushVerma2 <ayushverma2@gmail.com>
2020-02-12 16:15:34 +0800

    added schedule thread pool example

---
+++ src/main/java/Threads/TaskRejectionHandler.java
@@ -0,0 +1,29 @@
+package Threads;
+
+import java.util.concurrent.*;
+
+public class TaskRejectionHandler {
+
+    public static void main(String[] args) {
+
+        ExecutorService ses = new ThreadPoolExecutor(10, 20, 100,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(300),
+                new CustomeRejectionHandler());
+
+        // to shutdonw
+        ses.shutdown(); // it will initiate only
+
+    }
+
+
+}
+
+class CustomeRejectionHandler implements RejectedExecutionHandler {
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+
+        // do logging..
+        System.out.println("what need to eb done");
+    }
+}
