fbc55eb3fd0bfaee38dc6ae987eeadac7492749d
AyushVerma2 <ayushverma2@gmail.com>
2020-02-13 22:24:24 +0800

    Implemented Custom queue

---
+++ src/main/java/Threads/MyArrayBlockingQueue.java
@@ -0,0 +1,67 @@
+package Threads;
+
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Custom class that is basic Array blocking Queue implementation
+ * @param <T>
+ */
+public class MyArrayBlockingQueue<T> {
+
+    Lock lock = new ReentrantLock();
+    Condition empty = lock.newCondition();
+    Condition full = lock.newCondition();
+    int max = 10;
+    LinkedList<T> myqueue;
+
+    public MyArrayBlockingQueue(int size) {
+        this.max = size;
+        myqueue = new LinkedList<>();
+
+    }
+
+    /**
+     * This method is used to put and element in Q
+     * @param i
+     * @throws InterruptedException
+     */
+    void put(T i) throws InterruptedException {
+        try {
+            lock.lock();
+            while (myqueue.size() == max) {
+                full.await();
+            }
+
+            myqueue.add(i);
+            empty.signalAll();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    /**
+     * Apit to take an elelment from Q
+     * @return
+     * @throws InterruptedException
+     */
+    T take() throws InterruptedException {
+        try {
+            lock.lock();
+            while (myqueue.size() == 0) {
+                empty.await();
+            }
+
+            T data = myqueue.remove();
+            full.signalAll();
+            return data;
+        } finally {
+            lock.unlock();
+        }
+
+
+    }
+}
---
+++ src/main/java/Threads/Producer_Consumer_01.java
@@ -0,0 +1,79 @@
+package Threads;
+
+/**
+ * we have 2 producer adn two consumer
+ * and ArrayBlockingQueue so it is very simple
+ * Also i have implemented custome Custome BlockingQueue and used
+ */
+public class Producer_Consumer_01 {
+
+    static int input = 0;
+
+    public static void main(String a[]) {
+
+        // create a Blocking Q
+
+        // java Queue
+        // ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<>(10);
+
+        // implement my Custom Quue
+        MyArrayBlockingQueue<Integer> arrayBlockingQueue = new MyArrayBlockingQueue<>(10);
+
+
+        // producer code
+        Runnable target;
+        Thread p1 = new Thread(() ->
+        {
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(input++);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread p2 = new Thread(() ->
+        {
+            while (true) {
+                try {
+                    arrayBlockingQueue.put(input++);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        p1.start();
+        p2.start();
+
+
+        // consumer
+
+        Thread c1 = new Thread(() ->
+        {
+            while (true) {
+                try {
+                    int i = arrayBlockingQueue.take();
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread c2 = new Thread(() ->
+        {
+            while (true) {
+                try {
+                    int i = arrayBlockingQueue.take();
+                    System.out.println(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        c1.start();
+        c2.start();
+
+    }
+}
